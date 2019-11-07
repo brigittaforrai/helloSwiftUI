e43fefa6e8759ab31f480aef3fee62af6d9bc03a
brigittaforrai <forraibrigi@gmail.com>
2019-11-07 13:02:53 +0100

    Initial Commit

---
+++ swiftui1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui1
+//
+//  Created by brigitta forrai on 2019. 11. 07..
+//  Copyright © 2019. brigitta forrai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
